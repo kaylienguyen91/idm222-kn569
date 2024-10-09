48c100dbe0657bb848c21496c8ddf7d04cc95018
kaylienguyen91 <kn569@drexel.edu>
2024-10-09 00:28:41 -0400

    add personality in homepage

--- main.js
+++ main.js
@@ -14,6 +14,62 @@ document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', 
 }, false));
 
 
+// Running text in introduction in homepage
+
+var Text = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+Text.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-words');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new Text(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+};
+
+
+
 // create references to the modal...
 var modal = document.getElementById('myModal');
 // to all images -- note I'm using a class!
